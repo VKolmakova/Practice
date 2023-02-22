71a3b11457fbf90775d9a9b5a50fa98b97342b31
Valentina <kolmakovavalentina@mail.ru>
2023-02-22 22:08:56 +0300

    temp task for check

---
+++ Day_2/z1.1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter funt: ");
+double funt = double.Parse(Console.ReadLine());
+int gramsInKg = 1000;
+double gramsInFunt = 409.5;
+double kgInFunt = gramsInFunt / gramsInKg;
+Console.WriteLine(funt + " funt in kg =  " + funt * kgInFunt);
