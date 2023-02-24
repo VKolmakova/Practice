6fcebd811ba1375b68cdc8e40399b9d3d2c95f03
Valentina <kolmakovavalentina@mail.ru>
2023-02-24 11:51:04 +0300

    :)

--- Day_2/z6/Program.cs
+++ Day_2/z6/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        static void F(double a, double b, double m)
+        {
+            Console.WriteLine("Введите х");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double h = (b - a) / m;
+            for (double i = a; i <= b; i += 0.1)
+            {
+                Console.WriteLine(Math.Sin(Math.Pow(x, 2)));
+                x += h;
+            }
+        }
+        static void Main()
+        {
+            F(Math.PI / 6, 2*Math.PI / 3, 10);
+        }
+    }
+}
