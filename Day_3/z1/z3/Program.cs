d4b08091bd68372f9779c24c5e531bf3c4ea50b9
Valentina <kolmakovavalentina@mail.ru>
2023-02-25 10:40:53 +0300

    :)

--- Day_3/z1/z3/Program.cs
+++ Day_3/z1/z3/Program.cs
@@ -1,43 +1,97 @@
-﻿class Drob
+﻿class Fraction
 {
-    int chislitel;
-    int znamenatel;
-
-    public Drob() { }
-
-    public Drob(int chislit, int znamenat)
+    private int numerator;
+    private int denominator;
+    public double GetValue
     {
-        this.chislitel = chislit;
-        this.znamenatel = znamenat;
-    }
-
-    public int Chislitel
-    {
-        get { return chislitel; }
-        set { chislitel = value; }
-    }
-
-    public int Znamenatel
-    {
-        get { return znamenatel; }
-        set { znamenatel = value; }
-    }
-
-    public static Drob Plus(Drob drob1, Drob drob2)
-    {
-        Drob dr = new Drob();
-
-        if (drob1.Znamenatel != drob2.Znamenatel)
+        get
         {
-            dr.Chislitel = drob1.Chislitel * drob2.Znamenatel + drob2.Chislitel * drob1.Znamenatel;
-            dr.Znamenatel = drob1.Znamenatel * drob2.Znamenatel;
+            if (denominator == 0) return double.NaN;
+            return (double)numerator / denominator;
         }
-        else
+    }
+    public Fraction()
+    {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0) throw new DivideByZeroException("Делитель не может быть нулем");
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+    public Fraction(string stringFraction)
+    {
+        string[] sf = stringFraction.Split('/');
+        if (sf.Length < 2) throw new Exception("Не удается преобразовать строку в дробь");
+        if (sf[1] == "0") throw new DivideByZeroException("Делитель не может быть нулем");
+        int nr, dr;
+        if (!int.TryParse(sf[0], out nr) || !int.TryParse(sf[1], out dr))
+            throw new Exception("Не удается преобразовать строку в дробь");
+        this.numerator = nr;
+        this.denominator = dr;
+    }
+
+    override public string ToString()
+    {
+        if (denominator == 0) return "NaN";
+        return string.Format("{0}/{1}", numerator, denominator);
+    }
+
+    static public Fraction operator +(Fraction f1, Fraction f2)
+    {
+        int df = f1.denominator;
+        int nf = f1.numerator + f2.numerator;
+        if (f1.denominator != f2.denominator)
         {
-            dr.Chislitel = drob1.Chislitel + drob2.Chislitel;
-            dr.Znamenatel = drob1.Znamenatel;
+            df *= f2.denominator;
+            nf = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
         }
-        return dr;
+        return new Fraction(nf, df);
+    }
+
+    static public Fraction operator -(Fraction f1, Fraction f2)
+    {
+        int df = f1.denominator;
+        int nf = f1.numerator - f2.numerator;
+        if (f1.denominator != f2.denominator)
+        {
+            df *= f2.denominator;
+            nf = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+        }
+        return new Fraction(nf, df);
+    }
+
+    static public Fraction operator *(Fraction f1, Fraction f2)
+    {
+        return new Fraction(f1.numerator * f2.numerator, f1.denominator * f2.denominator);
+    }
+
+    static public Fraction operator /(Fraction f1, Fraction f2)
+    {
+        if (f2.numerator == 0) throw new DivideByZeroException("Деление на 0");
+        return f1 * f2.FlipFraction;
+    }
+
+    public Fraction FlipFraction { get { return new Fraction(denominator, numerator); } }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Fraction fr1 = new Fraction(1, 2);
+        Fraction fr2 = new Fraction("1/3");
+        Fraction fr3 = fr1 + fr2;
+        Console.WriteLine("{0} + {1} = {2} = {3}.", fr1, fr2, fr3, fr3.GetValue);
+        fr3 = fr1 - fr2;
+        Console.WriteLine("{0} - {1} = {2} = {3}.", fr1, fr2, fr3, fr3.GetValue);
+        fr3 = fr1 * fr2;
+        Console.WriteLine("{0} * {1} = {2} = {3}.", fr1, fr2, fr3, fr3.GetValue);
+        fr3 = fr1 / fr2;
+        Console.WriteLine("{0} / {1} = {2} = {3}.", fr1, fr2, fr3, fr3.GetValue);
+        Console.ReadLine();
     }
 }
 
