5c281d6fa4293b52e36a91a6801c9afefe388f54
Valentina <kolmakovavalentina@mail.ru>
2023-02-25 19:14:15 +0300

    :)

--- Day_3/z1/z3/Program.cs
+++ Day_3/z1/z3/Program.cs
@@ -1,23 +1,53 @@
-﻿class Fractions
+﻿class Fraction
 {
     public int firstNumerator;
     public int firstDenominator;
     public int secondNumerator;
     public int secondDenominator;
  
-    public Fractions(int firstNumerator, int firstDenominator, int secondNumeration, int secondDenominator)
+    public Fraction(int firstNumerator, int firstDenominator, int secondNumeration, int secondDenominator)
     {
         this.firstNumerator = firstNumerator; //Numerator - числитель
         this.firstDenominator = firstDenominator; // Denominator - знаменатель
         this.secondNumerator = secondNumeration;
         this.secondDenominator = secondDenominator;
     }
-    public void Multiply(int firstNumerator, int secondNumerator,int firstDenominator, int secondDenominator)
+    public void Multiply()
     {
         int numenator = (firstNumerator * secondNumerator);
         int denominator = (firstDenominator * secondDenominator);
-        Console.WriteLine($"Fraction = {numenator} / {denominator}");
+        Console.WriteLine($"Numltiply = {numenator} / {denominator}");
     }
+    public void Division()
+    {
+        int numenator = (firstNumerator * secondDenominator);
+        int denominator = (firstDenominator * secondNumerator);
+        Console.WriteLine($"Division = {numenator} / {denominator}");
+    }
+    public void Sum()
+    {
+        int commonDenominator = CommonDenominator(firstDenominator, secondDenominator);
+        int firstFinalNumerator = (commonDenominator / firstDenominator) * firstNumerator;
+        int secondFinalNumerator = (commonDenominator / secondDenominator) * secondNumerator;
+        Console.WriteLine($"Sum = {firstFinalNumerator+secondFinalNumerator} / {commonDenominator}");
+    }
+    public void Subtraction()
+    {
+        int commonDenominator = CommonDenominator(firstDenominator, secondDenominator);
+        int firstFinalNumerator = (commonDenominator / firstDenominator) * firstNumerator;
+        int secondFinalNumerator = (commonDenominator / secondDenominator) * secondNumerator;
+        Console.WriteLine($"Subtraction = {firstFinalNumerator - secondFinalNumerator} / {commonDenominator}");
+    }
+    private int CommonDenominator(int firstDenominator, int secondDenominator)
+    {
+        for (int i = firstDenominator; i < secondDenominator * firstDenominator; i += firstDenominator)
+        {
+            if (i % firstDenominator == 0 && i % secondDenominator == 0) return i;
+        }
+
+        return -1;
+    }
+
 }
 class Program
 {
@@ -31,9 +61,11 @@ class Program
         int secondNumerator = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter second Denominator ");
         int secondDenominator = Convert.ToInt32(Console.ReadLine());
-        Fractions tmp = new Fractions(firstNumerator, firstDenominator, secondNumerator, secondDenominator);
-        tmp.Multiply(firstNumerator, secondNumerator, firstDenominator, secondDenominator);
-       
+        Fraction fraction = new Fraction(firstNumerator, firstDenominator, secondNumerator, secondDenominator);
+        fraction.Multiply();
+        fraction.Division();
+        fraction.Sum();
+        fraction.Subtraction();
 
     }
 }
