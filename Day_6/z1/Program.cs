f9b244d5a377e84e7376fb188d9c95476f373081
Valentina <kolmakovavalentina@mail.ru>
2023-03-01 19:17:42 +0300

    :)

--- Day_6/z1/Program.cs
+++ Day_6/z1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[] {1,2,3,4,5,6};
+foreach (int i in array)
+{
+    Console.Write($"{i} ");
+}
+Console.WriteLine();
+Console.WriteLine("Answer: ");
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        array[i] = 0;
+    }
+    Console.Write(array[i] + " ");
+}
