d4845d4d799d2147429a2f4e4b9a643b87ffd7ae
Valentina <kolmakovavalentina@mail.ru>
2023-03-02 16:11:29 +0300

    :)

--- Day_7/z1/z2/Program.cs
+++ Day_7/z1/z2/Program.cs
@@ -1,12 +1,12 @@
-﻿    Console.WriteLine("Enter word length: ");
-    int n = int.Parse(Console.ReadLine());
-    Console.WriteLine("Enter the string: ");
-    string String = Console.ReadLine();
+﻿Console.WriteLine("Enter word length: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the string: ");
+string String = Console.ReadLine();
 
-    string[] words = String.Split(' ', ',', '.', ':', '?');
+string[] words = String.Split(' ', ',', '.', ':', '?');
 
-    var sortWords = from s in words where s.Length == n orderby s select s;
-    foreach (var s in sortWords)
-        Console.WriteLine(s);
+var sortWords = from s in words where s.Length == n orderby s select s;
+foreach (var s in sortWords)
+    Console.WriteLine(s);
 
-    Console.ReadKey(true);
+Console.ReadKey(true);
--- Day_8/z1/z1/Program.cs
+++ Day_8/z1/z1/Program.cs
@@ -1,25 +1,35 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Enter string : ");
-        string date = new string(Console.ReadLine());
-        FindData(date);
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Enter string : ");
+//        string date = new string(Console.ReadLine());
+//        FindData(date);
 
-        static void FindData(String date)
-        {
-            string dataRegex = @"\d{2}.\d{2}.\d{4}";
-            {
-                if (Regex.IsMatch(date, dataRegex))
-                {
-                    Console.WriteLine("True");
-                }
-                else Console.WriteLine("False");
-            }
-            
-        }
-    }
+//        static void FindData(String date)
+//        {
+//            string dataRegex = @"\d{2}.\d{2}.\d{4}";
+//            {
+//                if (Regex.IsMatch(date, dataRegex))
+//                {
+//                    Console.WriteLine("True");
+//                }
+//                else Console.WriteLine("False");
+//            }
+
+//        }
+//    }
+//}
+using System.Text.RegularExpressions;
+
+string pattern = @"([0-1][1-9]|2[0-9]|3[0-1]).(0[1-9]|1[0-2]).([1-2]+0+[0-1]+[0-9]|[1-2]+02+[0-3])";
+string input = Console.ReadLine();
+
+if (Regex.IsMatch(input, pattern))
+{
+    Console.WriteLine("Работает");
 }
+else Console.WriteLine("Не работает!");
